abc684a15a37c3e66dfde21b2c26fe95aa646d9d
503859505@qq.com <503859505@qq.com>
2019-01-04 17:53:48 +0800

    2019.1.4--model

--- src/components/model/model.vue
+++ src/components/model/model.vue
@@ -1,10 +1,21 @@
 <template>
-<div>1111</div>
+  <el-container class="page_main">
+    <el-header>导航栏</el-header>
+    <el-main>
+      <el-container>
+        <el-header>搜索</el-header>
+        <el-main>搜索结果/推荐</el-main>
+      </el-container>
+    </el-main>
+    <MyFooter></MyFooter>
+  </el-container>
 </template>
 
 <script>
+    import MyFooter from "../element/MyFooter";
     export default {
       name: "model",
+      components: {MyFooter},
       created() {
         this.model = this.$route.params.model;
         console.log(this.model);
@@ -21,5 +32,13 @@
 </script>
 
 <style scoped>
-
+  .el-footer{
+    padding: 0;
+  }
+  .el-header{
+    padding: 0;
+  }
+  .el-main{
+    padding: 0;
+  }
 </style>
